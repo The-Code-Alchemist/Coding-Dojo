0d328d2f1ffa52520a050520e9ed61db17a1f5a7
Onur Akgun <onur.akgun@code-alchemists.com>
2026-03-30 23:58:53 +0200

    ⚪ White Belt - Finalized setup, CLI execution and workspace foundation

---
+++ dojo-project/src/main/java/com/codealchemists/dojo/DojoApplication.java
@@ -0,0 +1,7 @@
+package com.codealchemists.dojo;
+
+public class DojoApplication {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Code Alchemists Dojo");
+    }
+}
