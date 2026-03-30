ec7cfe6a3dd6e347a62f9eb34f31d9f741d922fe
Onur Akgun <onur.akgun@code-alchemists.com>
2026-03-31 00:39:24 +0200

    🟡 Yellow Belt - Added endpoint testing and fixed assets. SpringBoot added. Finalized builder README with testing and CLI workflow

--- dojo-project/src/main/java/com/codealchemists/dojo/DojoApplication.java
+++ dojo-project/src/main/java/com/codealchemists/dojo/DojoApplication.java
@@ -1,7 +1,12 @@
 package com.codealchemists.dojo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class DojoApplication {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Code Alchemists Dojo");
+        SpringApplication.run(DojoApplication.class, args);
     }
 }
